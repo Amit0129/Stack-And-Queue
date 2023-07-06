4e3aee98b30b6871ed82ceca39b62d3f0756219c
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-06 23:35:02 +0530

    [Amit]Add : Adding Data to Queue

--- Stack_And_Queue/Stack_And_Queue/Program.cs
+++ Stack_And_Queue/Stack_And_Queue/Program.cs
@@ -4,42 +4,48 @@
     {
         static void Main(string[] args)
         {
-            StackMethods<int> stackMethods= new StackMethods<int>();
-            Console.WriteLine("Enter 1 to add element to Stack");
-            Console.WriteLine("Enter 2 to empty the Stack");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
+            StackMethods<int> stackMethods = new StackMethods<int>();
+            QueueMethods<int> queueMethods = new QueueMethods<int>();
+            bool check = true;
+            while (check)
             {
-                case 1:
-                    bool check = true;
-                    while (check)
-                    {
+                Console.WriteLine("Enter 1 to add element to Stack");
+                Console.WriteLine("Enter 2 to empty the Stack");
+                Console.WriteLine("Enter 3 to add element to Queue");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+
                         Console.WriteLine("Enter a number");
                         int valu = Convert.ToInt32(Console.ReadLine());
                         stackMethods.Add(valu);
-                        Console.WriteLine("Enter y for adding more data");
-                        string doOrNot = Console.ReadLine().ToLower();
-                        if (doOrNot == "y")
-                        {
-                            check = true;
-                        }
-                        else
-                        {
-                            check = false;
-                        }
-                    }
-                    stackMethods.Display();
-                    break;
-                case 2:
-                    stackMethods.Add(30);
-                    stackMethods.Add(56);
-                    stackMethods.Add(70);
-                    stackMethods.Display();
-                    stackMethods.EmptyStack();
-                    stackMethods.Display();
-                    break;
-                default:
-                    break;
+                        stackMethods.Display();
+                        break;
+                    case 2:
+                        stackMethods.EmptyStack();
+                        stackMethods.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter a number");
+                        int qValu = Convert.ToInt32(Console.ReadLine());
+                        queueMethods.Add(qValu);
+                        queueMethods.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Choose a valid choice");
+                        break;
+                }
+                Console.WriteLine("Enter y for adding more data");
+                string doOrNot = Console.ReadLine().ToLower();
+                if (doOrNot == "y")
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
             }
         }
     }
---
+++ Stack_And_Queue/Stack_And_Queue/QueueMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_And_Queue
+{
+    public class QueueMethods<T>
+    {
+        public Node<T> head;
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next= node;
+            }
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Empty Linked List");
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
